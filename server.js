5967d384fb01766c856c6b5fe8150d65971d8be2
myersg86 <myersg86@gmail.com>
2015-07-28 04:55:16 -0700

    Update server.js
    Set up dependency-free NodeJS server for static files

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  var html = "<!doctype html>" + 
-    "<html><head><title>Hello, world!</title></head>" +
-    "<body><h1>Hello, world!</h1></body></html>";
-  
-  res.writeHead(200, {
-    // set the type of content we're returning
-    "Content-Type": "text/html",   // set the length of our content
-    "Content-Length": html.length
-  });
-  // end the response, sending it and returning our HTML
-  res.end(html);
-}).listen(8000, "127.0.0.1");
+var fs = require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err,data) {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain'});
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode,
+                    { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+}
+
+http.createServer(function(req,res){
+   // normalize url by removing querystring
+   // trailing slash, and making lowercase
+   var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+   switch(path) {
+        case '':
+           serveStaticFile(res, '/public/home.html', 'text/html');
+           break;
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break
+        case 'img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+            break;
+        default:
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
+            break;
+   }
+}).listen(3000);
+
+console.log('Server started on localhost:4000; press Ctrl-C to terminate....');
